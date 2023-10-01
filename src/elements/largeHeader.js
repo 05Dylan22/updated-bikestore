437e16a1eb0a1d54f77492c16c89e05ab2b46d97
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-01 17:43:46 -0400

    large header handleIconClick func created

--- src/elements/largeHeader.js
+++ src/elements/largeHeader.js
@@ -1,19 +1,35 @@
+import { useState } from "react"
 import { imagesIcons } from "../App"
 import MiddleHeader from "./middleHeader"
 
-const LargeHeader = ({fillHeart, emptyHeart, heartHover}) => {
+const LargeHeader = ({fillHeart, emptyHeart, heartHover, openIcon, closeIconDisplay}) => {
   imagesIcons.heartOutline = heartHover
+  const [open, setOpen] = useState(false)
+  const [currentOpen, setCurrentOpen] = useState(null)
 
-
+  function handleIconClick (icon) {
+    if (!open) {
+      openIcon(icon)
+      setOpen(true)
+      setCurrentOpen(icon)
+    }
+    if (open && icon === currentOpen) {
+      closeIconDisplay()
+      setOpen(false)
+      setCurrentOpen(null)
+    }
+    setCurrentOpen(icon)
+    openIcon(icon)
+  }
 
   return (
     <header className="large-header">
-      <img onClick={handleIconClick} alt="business logo" className="large-logo" src={imagesIcons.logo}/>
+      <img onClick={() => handleIconClick("logo")} alt="business logo" className="large-logo" src={imagesIcons.logo}/>
       <MiddleHeader/>
-      <img onClick={handleIconClick} alt="search icon" className="large-search-icon" src={imagesIcons.searchIcon}/>
-      <img onClick={handleIconClick} onMouseLeave={emptyHeart} onMouseOver={fillHeart} alt="heart icon outline" className="large-wishlist-icon" src={imagesIcons.heartOutline}/>
-      <img onClick={handleIconClick} alt="profile icon" className="large-profile-icon" src={imagesIcons.profileIcon}/>
-      <img onClick={handleIconClick} alt="cart icon" className="large-cart-icon" src={imagesIcons.cartIcon}/>
+      <img onClick={() => handleIconClick("search")} alt="search icon" className="large-search-icon" src={imagesIcons.searchIcon}/>
+      <img onClick={() => handleIconClick("wishlist")} onMouseLeave={emptyHeart} onMouseOver={fillHeart} alt="heart icon outline" className="large-wishlist-icon" src={imagesIcons.heartOutline}/>
+      <img onClick={() => handleIconClick("profile")} alt="profile icon" className="large-profile-icon" src={imagesIcons.profileIcon}/>
+      <img onClick={() => handleIconClick("cart")} alt="cart icon" className="large-cart-icon" src={imagesIcons.cartIcon}/>
     </header>
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,12 @@
 import LargeHeader from "../elements/largeHeader"
 import SmallHeader from "../elements/smallHeader"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useReducer } from "react"
 import { imagesIcons } from "../App"
 import Wishlist from "../elements/wishlist"
 
 const Home = () => {
   const [heartHover, setHeartHover] = useState(imagesIcons.heartOutline)
+  const [displayIconEl, setDisplayIconEl] = useState()
 
   function fillHeart () {
     setHeartHover(imagesIcons.solidHeart)
@@ -31,9 +32,36 @@ const Home = () => {
 
   window.addEventListener("resize", checkScreenSize)
 
+  function openIcon (icon) {
+    switch (icon) {
+      case "logo":
+        setDisplayIconEl(<Wishlist/>)
+        break
+      case "wishlist":
+        setDisplayIconEl(<Wishlist/>)
+        break
+      case "profile":
+        setDisplayIconEl(<Wishlist/>)
+        break
+      case "cart":
+        setDisplayIconEl(<Wishlist/>)
+        break
+      case "search":
+        setDisplayIconEl(<Wishlist/>)
+        break
+      default:
+        console.log("default")
+    }
+  }
+
+  function closeIconDisplay (icon) {
+    setDisplayIconEl(null)
+  }
+
   return (
     <>
-      {largeScreen ? <LargeHeader fillHeart={fillHeart} emptyHeart={emptyHeart} heartHover={heartHover}/> : <SmallHeader fillHeart={fillHeart} emptyHeart={emptyHeart} heartHover={heartHover}/>}
+      {largeScreen ? <LargeHeader closeIconDisplay={closeIconDisplay} openIcon={openIcon} fillHeart={fillHeart} emptyHeart={emptyHeart} heartHover={heartHover}/> : <SmallHeader fillHeart={fillHeart} emptyHeart={emptyHeart} heartHover={heartHover}/>}
+      {displayIconEl}
     </>
   )
 }
