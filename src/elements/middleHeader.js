53e01402928d68c89dd54dae3dc39d2bd9794045
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-01 21:14:09 -0400

    large middle header hovers

--- src/elements/middleHeader.js
+++ src/elements/middleHeader.js
@@ -1,13 +1,64 @@
+import { useState } from "react"
+
 const MiddleHeader = () => {
+  const middleLinks = [
+    {
+      text: "Road",
+      key: "road"
+    },
+
+    {
+      text: "Electric",
+      key: "electric"
+    },
+
+    {
+      text: "BMX",
+      key: "bmx"
+    },
+
+    {
+      text: "Parts",
+      key: "parts"
+    },
+
+    {
+      text: "Accessories",
+      key: "accessories"
+    },
+
+    {
+      text: "Apparel",
+      key: "apparel"
+    }
+  ]
+
+  function handleHover (e) {
+    e.target.nextElementSibling.classList = "large-middle-links-hover"
+  }
+
+  function handleHoverDropdown (e) {
+    e.target.classList = "large-middle-links-hover"
+  }
+
+  function mouseLeave (e) {
+    e.target.nextElementSibling.classList = "none"
+  }
+
+  function mouseLeaveDropdown (e) {
+    e.target.classList = "none"
+  }
+
   return (
   <nav className="large-middle-header">
-    <p className="large-header-links">Mountain</p>
-    <p className="large-header-links">Road</p>
-    <p className="large-header-links">Electric</p>
-    <p className="large-header-links">BMX</p>
-    <p className="large-header-links">Parts</p>
-    <p className="large-header-links">Accessories</p>
-    <p className="large-header-links">Apparel</p>
+    {middleLinks.map((link) => {
+      return (
+        <div key={link.key} className="large-links-div">
+          <p onMouseLeave={(e) => {mouseLeave(e)}} onMouseOver={(e) => {handleHover(e)}} className="large-header-links">{link.text}</p>
+          <div onMouseLeave={(e) => {mouseLeaveDropdown(e)}} onMouseOver={(e) => {handleHoverDropdown(e)}} className="none">Hover</div>
+        </div>
+      )
+    })}
   </nav>
   )
 }
