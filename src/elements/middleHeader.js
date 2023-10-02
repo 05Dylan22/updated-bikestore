b9811cecface9b9ddda890e6279031876be398f7
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-02 12:04:59 -0400

    hover content works

--- src/elements/middleHeader.js
+++ src/elements/middleHeader.js
@@ -1,72 +1,237 @@
+import { useRef, useState } from "react"
+
 const MiddleHeader = () => {
+  const subMountain = [
+    {
+      text: "Full Suspension",
+      key: "full"
+    },
+
+    {
+      text: "Hardtail",
+      key: "hard"
+    },
+
+    {
+      text: "Cross Country",
+      key: "cross"
+    },
+
+    {
+      text: "Trail",
+      key: "trail"
+    },
+
+    {
+      text: "Enduro",
+      key: "enduro"
+    },
+
+    {
+      text: "Downhill",
+      key: "down"
+    },
+
+    {
+      text: "Fat Bikes",
+      key: "fat"
+    }
+  ]
+  const subRoad = [
+    {
+      text: "Performance",
+      key: "perf"
+    },
+
+    {
+      text: "Gravel",
+      key: "grav"
+    },
+
+    {
+      text: "Triathelon",
+      key: "triath"
+    }
+  ]
+  const subParts = [
+    {
+      text: "Wheels, Tires, Tubes",
+      key: "wtt"
+    },
+
+    {
+      text: "Road Wheels",
+      key: "rw"
+    },
+
+    {
+      text: "Mountain Wheels",
+      key: "mw"
+    },
+
+    {
+      text: "Road Tires",
+      key: "rt"
+    },
+
+    {
+      text: "Mountain Tires",
+      key: "mt"
+    },
+
+    {
+      text: "Gravel Tires",
+      key: "gt"
+    },
+
+    {
+      text: "City Tires",
+      key: "ct"
+    },
+
+    {
+      text: "Tubes",
+      key: "tubes"
+    },
+
+    {
+      text: "Tubeless Accessories",
+      key: "ta"
+    }
+  ]
+  const subApparel = [
+    {
+      text: "Pants",
+      key: "pants"
+    },
+
+    {
+      text: "Jackets",
+      key: "jackets"
+    },
+
+    {
+      text: "Helmets",
+      key: "helmets"
+    },
+
+    {
+      text: "Shoes",
+      key: "shoes"
+    }
+  ]
+  const subAccessories = [
+    {
+      text: "Pumps",
+      key: "pumps"
+    },
+
+    {
+      text: "Locks",
+      key: "locks"
+    },
+
+    {
+      text: "Bike Racks",
+      key: "Bracks"
+    },
+
+    {
+      text: "Car Racks",
+      key: "Cracks"
+    },
+
+    {
+      text: "Bike Storage",
+      key: "storage"
+    }
+  ]
+  const subElectric = [
+    {
+      text: "City & Hybrid",
+      key: "city"
+    },
+
+    {
+      text: "Commuter",
+      key: "commute"
+    },
+
+    {
+      text: "Recreation",
+      key: "recre"
+    }
+  ]
+
   const middleLinks = [
     {
       text: "Road",
-      key: "road"
+      subCat: subRoad,
+      key: "subRoad"
     },
 
     {
       text: "Electric",
+      subCat: subElectric,
       key: "electric"
     },
 
     {
-      text: "BMX",
-      key: "bmx"
+      text: "Mountain",
+      subCat: subMountain,
+      key: "mtn"
     },
 
     {
       text: "Parts",
+      subCat: subParts,
       key: "parts"
     },
 
     {
       text: "Accessories",
+      subCat: subAccessories,
       key: "accessories"
     },
 
     {
       text: "Apparel",
+      subCat: subApparel,
       key: "apparel"
     }
   ]
 
-  const subMountain = []
-  const subRoad = []
-  const subBMX = []
-  const subParts = []
-  const subApparel = []
-  const subAccessories = []
+  const [isHovering, setIsHovering] = useState(false)
+  const [hoverDiv, setHoverDiv] = useState(null)
 
-  function handleHover (e) {
-    e.target.nextElementSibling.classList = "large-middle-links-hover"
+  function mouseEntered (e, list) {
+    setIsHovering(true)
+    setHoverDiv(<HoverContent list={list}/>)
   }
 
-  function handleHoverDropdown (e) {
-    e.target.classList = "large-middle-links-hover"
-  }
-
-  function mouseLeave (e) {
-    e.target.nextElementSibling.classList = "none"
-  }
-
-  function mouseLeaveDropdown (e) {
-    e.target.classList = "none"
+  function mouseLeft (e) {
+    setIsHovering(false)
   }
 
   return (
-  <nav className="large-middle-header">
+  <nav onMouseLeave={(e) => {mouseLeft(e)}} className="large-middle-header">
     {middleLinks.map((link) => {
       return (
-        <div key={link.key} className="large-links-div">
-          <p onMouseLeave={(e) => {mouseLeave(e)}} onMouseOver={(e) => {handleHover(e)}} className="large-header-links">{link.text}</p>
-          <div onMouseLeave={(e) => {mouseLeaveDropdown(e)}} onMouseOver={(e) => {handleHoverDropdown(e)}} className="none">
-
-          </div>
+        <div onMouseEnter={(e) => {mouseEntered(e, link.subCat)}} key={link.key} className="large-links-div">
+          <p className="large-header-links">{link.text}</p>
         </div>
       )
     })}
+    {isHovering && hoverDiv}
   </nav>
+  )
+}
+
+const HoverContent = ({ list }) => {
+  return (
+    <div className="large-middle-links-hover">
+      {list.map(item => <h1 key={item.key}>{item.text}</h1>)}
+    </div>
   )
 }
 
