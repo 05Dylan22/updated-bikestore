993158e83defc4f0984457dd37898f41cd80525f
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-04 21:28:21 -0400

    wishlist item design large complete

--- src/elements/wishlistItem.js
+++ src/elements/wishlistItem.js
@@ -1,10 +1,12 @@
 const WishlistItem = ({ name, price }) => {
   return (
     <div className="wishlist-item">
-      <img src="" alt="" />
-      <p>{name}</p>
-      <p>{price}</p>
-      <img src="" alt="" />
+      <img className="wishlist-item-img" src={require("../images+icons/profile-collageimg2.jpg")} alt="temp" />
+      <div className="item-info">
+        <p className="wishlist-item-title">Here Is A More Bike</p>
+        <p className="wishlist-item-desc">This is where the description goes. it will need to be capped off at some point. more text pladfasf</p>
+        <p className="wishlist-item-price">${price}</p>
+      </div>
     </div>
   )
 }
