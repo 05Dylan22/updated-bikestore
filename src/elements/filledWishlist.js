253c7b625d474676bc1c1077373e3882a859994d
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-05 10:00:17 -0400

    filled wishlist responsive

--- src/elements/filledWishlist.js
+++ src/elements/filledWishlist.js
@@ -3,6 +3,8 @@ import WishlistItem from "./wishlistItem"
 const FilledWishlist = ({ items }) => {
   return (
     <div className="icon-container">
+      <p className="wishlist-title">My Wishlist</p>
+      <p className="wishlist-subtitle">You Have {items.length} Item(s) In Your Wishlist</p>
       {items.map(item => <WishlistItem key={item.key} name={item.name} price={item.price}/>)}
     </div>
   )
