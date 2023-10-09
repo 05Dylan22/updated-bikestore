8da10226849b786e768f54cbd0a1dddebb7293ef
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-09 01:28:38 -0400

    sub cat shows alone

--- src/elements/hamburgerMenus.js
+++ src/elements/hamburgerMenus.js
@@ -7,16 +7,24 @@ import { subRoad, subAccessories, subApparel, subElectric,subMountain,subParts }
 const HamburgerMenus = ({dispatch}) => {
   const [subCategory, setSubCategory] = useState(null)
   const [isSubCategory, setIsSubCategory] = useState(false)
+
   function hamburgCategoryClick (category) {
     setSubCategory(<SubCategoryMenu items={category}/>)
     setIsSubCategory(true)
   }
+
+  function closeHamburger() {
+    dispatch({payload: {identifier: "HAMBURGERMENU", component: <HamburgerMenus/>}})
+    setIsSubCategory(false)
+  }
+
   return (
     <>
-      {<div className="hamburger-menus-container">
+      {!isSubCategory ? 
+      <div className="hamburger-menus-container">
         <div className="top-hamburger">
           <img className="hamburger-logo" src={imagesIcons.logo} alt="logo" />
-          <img onClick={() => dispatch({payload: {identifier: "HAMBURGERMENU", component: <HamburgerMenus/>}})} className="close-hamburger" src={require("../images+icons/search.cancel.icon.png")} alt="close menu" />
+          <img onClick={closeHamburger} className="close-hamburger" src={require("../images+icons/search.cancel.icon.png")} alt="close menu" />
         </div>
         <p onClick={() => hamburgCategoryClick(subRoad)} className="menu-main-section-p">Road<span>&gt;</span></p>
         <p onClick={() => hamburgCategoryClick(subElectric)} className="menu-main-section-p">Electric<span>&gt;</span></p>
@@ -24,8 +32,11 @@ const HamburgerMenus = ({dispatch}) => {
         <p onClick={() => hamburgCategoryClick(subParts)} className="menu-main-section-p">Parts<span>&gt;</span></p>
         <p onClick={() => hamburgCategoryClick(subAccessories)} className="menu-main-section-p">Accessories<span>&gt;</span></p>
         <p onClick={() => hamburgCategoryClick(subApparel)} className="menu-main-section-p">Apparel<span>&gt;</span></p>
+      </div>
+      : <div className="hamburger-menus-container">
         {subCategory}
-      </div>}
+      </div>
+      }
     </>
   )
 }
