029d2b2efee4d32812bc0047f33355bffe019f80
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-10 12:04:13 -0400

    created context

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -2,9 +2,23 @@ import { createContext, useContext, useReducer } from "react";
 
 const DataContext = createContext({})
 
+function reducer (state, action) {
+  if (!state.open) {
+    return {open: true, currentOpen: action.payload.identifier, displayIconEl: action.payload.component}
+  }
+  if (state.open && state.currentOpen === action.payload.identifier) {
+    return {open: false, currentOpen: null, displayIconEl: null}
+  }
+  if (state.open && state.currentOpen !== action.payload.identifier) {
+    return {...state, currentOpen: action.payload.identifier, displayIconEl: action.payload.component}
+  }
+}
+
 export const DataProvider = ({children}) => {
+  const [state, dispatch] = useReducer(reducer, {open: false, currentOpen: null, displayIconEl: null})
+
   return (
-    <DataContext.Provider value={{}}>
+    <DataContext.Provider value={{dispatch, state}}>
       {children}
     </DataContext.Provider>
   )
