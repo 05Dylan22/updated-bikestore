c4bdefdb6a66baa1194d1d8c53c4863e94bfb28a
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-27 16:29:14 -0400

    wishlist finished

--- src/elements/wishlistItem.js
+++ src/elements/wishlistItem.js
@@ -1,20 +1,28 @@
 import { useDispatch } from "react-redux"
 import { removeItem } from "../redux/handleWishlist"
 import { imagesIcons } from "../App"
+import { useRef } from "react"
 
 const WishlistItem = ({ name, price, desc, img, bike }) => {
-  //ADD WISHLIST HEART TOP RIGHT TO REMOVE FROM WISHLIST
   const dispatch = useDispatch()
+  const WishlistItem = useRef(null)
+
+  function removeWishlistItem(e) {
+    WishlistItem.current.classList.add("wishlist-item-leaving")
+    setTimeout(() => {
+      dispatch(removeItem(bike))
+    },550)
+  }
   return (
-    <div className="wishlist-item">
-      <img onClick={() => dispatch(removeItem(bike))} className="wishlist-item-heart" src={imagesIcons.solidHeart} alt="solid heart" />
+    <div ref={WishlistItem} className="wishlist-item">
+      <img onClick={(e) => removeWishlistItem(e)} className="wishlist-item-heart" src={imagesIcons.solidHeart} alt="solid heart" />
       <img loading="lazy" className="wishlist-item-img" src={img} alt="temp" />
       <div className="item-info">
         <p className="wishlist-item-title">{name}</p>
         <p className="wishlist-item-desc">{desc}</p>
         <div className="bottom-wishlist-item">
           <p className="wishlist-item-price">{price}</p>
-          <button onClick={() => dispatch(removeItem(bike))} className="wishlist-add-to-cart">Add To Cart</button>
+          <button onClick={(e) => removeWishlistItem(e)} className="wishlist-add-to-cart">Add To Cart</button>
         </div>
       </div>
     </div>
