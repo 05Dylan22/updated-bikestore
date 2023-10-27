1a63b9f67f6e09772a224a140bfe87e1f778454f
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-27 13:58:51 -0400

    cart btn added and removes from wishlist non style

--- src/elements/filledWishlist.js
+++ src/elements/filledWishlist.js
@@ -10,7 +10,7 @@ const FilledWishlist = () => {
     <div className="icon-container">
       <p className="wishlist-title">My Wishlist</p>
       <p className="wishlist-subtitle">You Have {totalItems} Item(s) In Your Wishlist</p>
-      {bikes.map(bike => <WishlistItem key={wishlistContents[bike].id} name={wishlistContents[bike].name} price={wishlistContents[bike].price} desc={wishlistContents[bike].description}/>)}
+      {bikes.map(bike => <WishlistItem key={wishlistContents[bike].id} name={wishlistContents[bike].name} price={wishlistContents[bike].price} desc={wishlistContents[bike].description} img={wishlistContents[bike].images[0]} bike={wishlistContents[bike]}/>)}
     </div>
   )
 }
--- src/elements/wishlistItem.js
+++ src/elements/wishlistItem.js
@@ -1,13 +1,19 @@
-const WishlistItem = ({ name, price, desc }) => {
-  //ADD BUTTON TO RIGHT OF PRICE TO ADD TO CART
+import { useDispatch } from "react-redux"
+import { removeItem } from "../redux/handleWishlist"
+
+const WishlistItem = ({ name, price, desc, img, bike }) => {
+  //ADD WISHLIST HEART TOP RIGHT TO REMOVE FROM WISHLIST
+  const dispatch = useDispatch()
   return (
     <div className="wishlist-item">
-      <img loading="lazy" className="wishlist-item-img" src={require("../images+icons/profile-collageimg2.jpg")} alt="temp" />
+      <img loading="lazy" className="wishlist-item-img" src={img} alt="temp" />
       <div className="item-info">
         <p className="wishlist-item-title">{name}</p>
         <p className="wishlist-item-desc">{desc}</p>
-        <p className="wishlist-item-price">{price}</p>
-        <button>Add To Cart</button>
+        <div className="bottom-wishlist-item">
+          <p className="wishlist-item-price">{price}</p>
+          <button onClick={() => dispatch(removeItem(bike))} className="wishlist-add-to-cart">Add To Cart</button>
+        </div>
       </div>
     </div>
   )
