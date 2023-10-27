285afedda3801d113cb5ce327226c1d2c3f6791d
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-27 14:02:34 -0400

    added wishlist heart functionality, non styled

--- src/elements/wishlistItem.js
+++ src/elements/wishlistItem.js
@@ -1,11 +1,13 @@
 import { useDispatch } from "react-redux"
 import { removeItem } from "../redux/handleWishlist"
+import { imagesIcons } from "../App"
 
 const WishlistItem = ({ name, price, desc, img, bike }) => {
   //ADD WISHLIST HEART TOP RIGHT TO REMOVE FROM WISHLIST
   const dispatch = useDispatch()
   return (
     <div className="wishlist-item">
+      <img onClick={() => dispatch(removeItem(bike))} className="wishlist-item-heart" src={imagesIcons.solidHeart} alt="solid heart" />
       <img loading="lazy" className="wishlist-item-img" src={img} alt="temp" />
       <div className="item-info">
         <p className="wishlist-item-title">{name}</p>
