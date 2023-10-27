cc47b4263bc07a43ec522468929b6836b2abef5e
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-10-27 17:50:50 -0400

    header hearts finished

--- src/elements/smallHeader.js
+++ src/elements/smallHeader.js
@@ -2,15 +2,22 @@ import HamburgerMenu from "./hamburgerMenu"
 import { imagesIcons } from "../App"
 import { handleIcons } from "../redux/handleIcons"
 import { useDispatch, useSelector } from "react-redux"
+import { clickedHeart, leftHeart, overHeart } from "../redux/headerHearts"
 
 const SmallHeader = () => {
   const displayIconEl = useSelector((state) => state.handleIcons.displayIconEl)
+  const heart = useSelector((state) => state.handleHearts.heart)
   const dispatch = useDispatch()
+
+  function clickedHeartIcon() {
+    dispatch(handleIcons({identifier: "WISHLIST"}))
+    dispatch(clickedHeart())
+  }
 
   return (
     <header className="small-header">
       <HamburgerMenu/>
-      <img onClick={() => {dispatch(handleIcons({identifier: "WISHLIST"}))}} alt="heart icon outline" className="small-wishlist-icon" src={imagesIcons.heartOutline}/>
+      <img onMouseLeave={() => dispatch(leftHeart())} onMouseEnter={() => dispatch(overHeart())} onClick={() => clickedHeartIcon()} alt="heart icon outline" className="small-wishlist-icon" src={heart}/>
       <img alt="business logo" className="small-logo" src={imagesIcons.logo}/>
       <img onClick={() => {dispatch(handleIcons({identifier: "SEARCH"}))}} alt="search icon" className="small-search-icon" src={imagesIcons.searchIcon}/>
       <img onClick={() => {dispatch(handleIcons({identifier: "PROFILE"}))}} alt="profile icon" className="small-profile-icon" src={imagesIcons.profileIcon}/>
