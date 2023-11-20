258db91cab3046cad85ad98805e2ee71ed31b407
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-11-20 18:14:34 -0500

    cart and wishlist counter tracker in large header

--- src/elements/largeHeader.js
+++ src/elements/largeHeader.js
@@ -8,6 +8,8 @@ import { clickedHeart, overHeart, leftHeart } from "../redux/headerHearts"
 
 const LargeHeader = () => {
   const { displayIconEl } = useSelector((state) => state.handleIcons)
+  const cartContents = useSelector((state) => state.handleCart.cartContents)
+  const wishlistContents = useSelector((state) => state.handleWishlist.wishlistContents)
   const heart = useSelector((state) => state.handleHearts.heart)
   const dispatch = useDispatch()
 
@@ -23,9 +25,15 @@ const LargeHeader = () => {
       </Link>
       <MiddleHeader/>
       <img onClick={() => dispatch(handleIcons({identifier: "SEARCH"}))} alt="search icon" className="large-search-icon" src={imagesIcons.searchIcon}/>
-      <img onMouseLeave={() => dispatch(leftHeart())} onMouseEnter={() => dispatch(overHeart())} onClick={() => clickedHeartIcon()} alt="heart icon outline" className="large-wishlist-icon" src={heart}/>
+      <div className="heart-cart-container">
+        {Object.keys(wishlistContents).length > 0 && <p className="heart-cart-counter">{Object.keys(wishlistContents).length}</p>}
+        <img onMouseLeave={() => dispatch(leftHeart())} onMouseEnter={() => dispatch(overHeart())} onClick={() => clickedHeartIcon()} alt="heart icon outline" className="large-wishlist-icon" src={heart}/>
+      </div>
       <img onClick={() => dispatch(handleIcons({identifier: "PROFILE"}))} alt="profile icon" className="large-profile-icon" src={imagesIcons.profileIcon}/>
-      <img onClick={() => dispatch(handleIcons({identifier: "CART"}))} alt="cart icon" className="large-cart-icon" src={imagesIcons.cartIcon}/>
+      <div className="heart-cart-container">
+        {Object.keys(cartContents).length > 0 && <p className="heart-cart-counter">{Object.keys(cartContents).length}</p>}
+        <img onClick={() => dispatch(handleIcons({identifier: "CART"}))} alt="cart icon" className="large-cart-icon" src={imagesIcons.cartIcon}/>
+      </div>
       {displayIconEl}
     </header>
   )
