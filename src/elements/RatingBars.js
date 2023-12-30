fe85b46675a1f8be07528075ac4fe43f2651b3e2
Dylan Anderson <anderson.dylan.522@gmail.com>
2023-12-29 20:57:20 -0500

    rating bars working

--- src/elements/RatingBars.js
+++ src/elements/RatingBars.js
@@ -1,5 +1,17 @@
-const RatingBars = () => {
+const RatingBars = ({bike}) => {
   const starsOrder = [5, 4, 3, 2, 1]
+
+  //we need to organize the reviews array that is inside the bike object
+  //we need to create an object that will store the amount of reviews left for each star value
+  //use reduce method
+
+  const ratings = bike.reviews.reduce((groupedRatings, review) => {
+    const rating = review.rating
+    if (groupedRatings[rating] === undefined) groupedRatings[rating] = []
+    groupedRatings[rating].push(review)
+    return groupedRatings
+  }, {})
+
   return (
     <>
       {
@@ -7,8 +19,8 @@ const RatingBars = () => {
           <div className="rating-bar-sect" key={starNum}>
             <p className="rating-bar-head">{starNum} Stars</p>
             <div className="bar-and-percentage">
-              <div className="star-rating-bar"></div>
-              <p className="bar-filled-percentage">100%</p>
+              <div className="star-rating-bar" style={{backgroundImage: `linear-gradient(to right, rgb(255,164,28) ${ratings[starNum] ? (ratings[starNum].length / bike.reviews.length) * 100 : 0}%, white ${ratings[starNum] ? (ratings[starNum].length / bike.reviews.length) * 100 : 0}% 100%)`}}></div>
+              <p className="bar-filled-percentage">{ratings[starNum] ? (ratings[starNum].length / bike.reviews.length) * 100 : 0}%</p>
             </div>
           </div>
         ))
--- src/elements/ReviewSection.js
+++ src/elements/ReviewSection.js
@@ -2,14 +2,14 @@ import "../styles/reviewSect.css"
 import Review from "./Review"
 import RatingBars from "./RatingBars"
 
-const ReviewSection = () => {
+const ReviewSection = ({bike}) => {
 
   return (
     <section className="review-section">
       <div className="top-review-sect-div">
         <div>
           <h2 className="review-sect-heading">Customer Reviews</h2>
-          <RatingBars/>
+          <RatingBars bike={bike}/>
         </div>
         <div className="leave-review-div">
           <h2 className="leave-review-title">Satisfied With Your Purchase? Let Us Know How It Went.</h2>
