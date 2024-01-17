4c745f416945e07756fe33792b61b773d52f3fa0
Dylan Anderson <anderson.dylan.522@gmail.com>
2024-01-16 20:49:13 -0500

    blog posts images

--- src/elements/blogPost.js
+++ src/elements/blogPost.js
@@ -2,24 +2,24 @@ const BlogPost = () => {
   return (
     <section className="blogs-section">
       <div className="blog-container">
-        <img className="blog-img" src="https://th.bing.com/th/id/OIP.4usnnQpMLGmrv6dGK16qsgHaE7?pid=ImgDet&rs=1" alt="" />
-        <p className="blog-desc">Here is the description of the blog content</p>
+        <img className="blog-img" src="https://cdn.outsideonline.com/wp-content/uploads/2023/12/2023_MTB_Nebo_MarkMatthews_JLM_4551-scaled-e1703773749282.jpg?crop=16:9&width=1200&enable=upscale" alt="" />
+        <p className="blog-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
       <div className="blog-container">
-        <img className="blog-img" src="https://th.bing.com/th/id/OIP.4usnnQpMLGmrv6dGK16qsgHaE7?pid=ImgDet&rs=1" alt="" />
-        <p className="blog-desc">Here is the description of the blog content</p>
+        <img className="blog-img" src="https://www.australiangeographic.com.au/wp-content/uploads/2020/07/MJW2215.jpg" alt="" />
+        <p className="blog-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
       <div className="blog-container">
-        <img className="blog-img" src="https://th.bing.com/th/id/OIP.4usnnQpMLGmrv6dGK16qsgHaE7?pid=ImgDet&rs=1" alt="" />
-        <p className="blog-desc">Here is the description of the blog content</p>
+        <img className="blog-img" src="https://th.bing.com/th/id/R.f4a3f9a427d16651baec03980fc17928?rik=uIVYatHFIjL4UQ&riu=http%3a%2f%2fsportycious.com%2fwp-content%2fuploads%2f2014%2f12%2fMountain-Biking.jpg&ehk=MNNKVgOrWf7Bvg7QH7M5%2bn3v2EhvBY25SLUT%2f6LTVF4%3d&risl=&pid=ImgRaw&r=0" alt="" />
+        <p className="blog-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
       <div className="blog-container">
-        <img className="blog-img" src="https://th.bing.com/th/id/OIP.4usnnQpMLGmrv6dGK16qsgHaE7?pid=ImgDet&rs=1" alt="" />
-        <p className="blog-desc">Here is the description of the blog content</p>
+        <img className="blog-img" src="https://th.bing.com/th/id/R.fe3e4f409cee55c6e1b28f7cc8fdb382?rik=sJUjRxiR%2fJzS%2bA&riu=http%3a%2f%2fdigitalresult.com%2fwp-content%2fuploads%2f2015%2f09%2fmountain-biking-hd-wallpapers-14.jpeg&ehk=DSGNHitDzBTtpo3sks4SoJ7gEvyB7Ue3a7O%2f3o6EVbk%3d&risl=&pid=ImgRaw&r=0" alt="" />
+        <p className="blog-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
       <div className="blog-container">
-        <img className="blog-img" src="https://th.bing.com/th/id/OIP.4usnnQpMLGmrv6dGK16qsgHaE7?pid=ImgDet&rs=1" alt="" />
-        <p className="blog-desc">Here is the description of the blog content</p>
+        <img className="blog-img" src="https://th.bing.com/th/id/OIP.sA5IzeQ10cIaLyBqwevRMgHaE8?rs=1&pid=ImgDetMain" alt="" />
+        <p className="blog-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
     </section>
   )
